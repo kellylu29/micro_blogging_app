2c1b39bbd423fcd45330bf642b1bade6c82b3070
Kelly <kellylovelu@gmail.com>
2017-08-11 15:55:15 -0400

    view last 10 posts

--- db/migrate/20170811151127_create_posts_table.rb
+++ db/migrate/20170811151127_create_posts_table.rb
@@ -3,8 +3,8 @@ class CreatePostsTable < ActiveRecord::Migration[5.1]
   	create_table :posts do |t|
   		t.string :content
   		t.string :username
-      	t.string :title
-      	t.string :user_id
+      t.string :title
+      t.string :user_id
   	end
   end
 end
